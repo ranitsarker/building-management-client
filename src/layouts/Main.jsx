927f1411be5cec8df332aef9170a4f4a792bb1b2
ranitsarker <rntprince@gmail.com>
2023-11-23 21:33:47 +0600

    I have created navbar one shared container and home page create

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../compoments/Shared/Navbar/Navbar";
 
 const Main = () => {
     return (
         <>
-        That is main layout
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </>
     );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Container from "../../compoments/Shared/Container";
+
+const Home = () => {
+    return (
+        <>
+        <Container>
+        This is home page. 
+        </Container>
+
+        </>
+    );
+};
+
+export default Home;
