7b27c22549da6daf1d290757eba6bcff3404d9c1
ranitsarker <rntprince@gmail.com>
2023-11-23 20:43:08 +0600

    react router setup and tailwind setup done

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <>
+        That is main layout
+            <Outlet></Outlet>
+        </>
+    );
+};
+
+export default Main;
